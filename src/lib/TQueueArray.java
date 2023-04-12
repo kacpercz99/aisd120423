34b412103fe050cb39c9dfbedeedb3c4afd953ff
kacpercz99 <kacpercz99@student.kul.lublin.pl>
2023-04-12 18:26:54 +0200

    Changes by Administrator

--- src/lib/TQueueArray.java
+++ src/lib/TQueueArray.java
@@ -119,7 +119,11 @@ public class TQueueArray<E> implements InterQueueIterableArray<E> {
         sb.append("\n");
         return sb.toString();
     }
-    public E reset(){
-        
+    public E reset(E element){
+        E newHead=null;
+        while(!isEmpty()){
+            
+        }
+        return newHead;
     }
 }
